65967bd9031210e0d1a72d394e487e7ace899031
ravenkmusic <ravenkbeats@gmail.com>
2022-03-14 12:20:46 -0400

    simplified still has questions function

--- quiz_brain.py
+++ quiz_brain.py
@@ -9,4 +9,14 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Question {self.question_number}: {current_question.text} True or false?: ")
+        user_answer = input(f"Question {self.question_number}: {current_question.text} True or false?: ")
+        self.check_answer (user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        score = 0
+        if user_answer.lower() == correct_answer.lower():
+            score += 1
+            print("You are correct!")
+        else:
+            print("Your answer is incorrect.")
+        print(f"The answer was {correct_answer}.")
