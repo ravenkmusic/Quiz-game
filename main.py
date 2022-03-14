3fee4b31ebe64ecf86f8e3fa11ae172a54e582e2
ravenkmusic <ravenkbeats@gmail.com>
2022-03-14 11:54:05 -0400

    added new method that will generate new question for user after they answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -7,5 +8,3 @@ for question in question_data:
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
-
-print(question_bank)
---
+++ quiz_brain.py
@@ -0,0 +1,8 @@
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        input(f"Question:{self.question_number}: {current_question.text} True or false?: ")
