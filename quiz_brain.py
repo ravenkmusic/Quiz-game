99ae77c3e817b047de314933e4756737a12baf9e
ravenkmusic <ravenkbeats@gmail.com>
2022-03-14 12:44:46 -0400

    added check answer function to keep track of user's score

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,6 +1,7 @@
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = q_list
 
     def still_has_questions(self):
@@ -13,10 +14,10 @@ class QuizBrain:
         self.check_answer (user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
-        score = 0
         if user_answer.lower() == correct_answer.lower():
-            score += 1
+            self.score += 1
             print("You are correct!")
         else:
             print("Your answer is incorrect.")
         print(f"The answer was {correct_answer}.")
+        print(f"Your score is: {self.score} out of {self.question_number}.")
