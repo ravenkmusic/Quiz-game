91daeffff80510d938f03668df1c327d4aa42ad2
ravenkmusic <ravenkbeats@gmail.com>
2022-03-14 11:14:36 -0400

    looped through all questions to test retrieval ability from different modules

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from question_model import Question
 from data import question_data
 
-for item in question_data:
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+print(question_bank)
--- question_model.py
+++ question_model.py
@@ -1,4 +1,5 @@
 class Question:
-    def __init__ (self):
-        self.text
-        self.answer
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
